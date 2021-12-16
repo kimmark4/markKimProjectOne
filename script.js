2a56c43b4403a854228ed4ac3db91c40f9ed85bb
Mark <mark.kimk622@gmail.com>
2021-12-15 21:21:56 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -16,22 +16,22 @@ commentApp.formElement.addEventListener('submit', function (event) {
     if (comment) {
         const divElement = document.createElement('div');
         divElement.classList.add('commentFlex');
-        commentApp.addComments.appendChild(divElement)
+        commentApp.addComments.appendChild(divElement);
 
 
         const imgElement = document.createElement('img');
         imgElement.src = "../project-1-textilee/textilee-assets/comment-2.jpg";
-        divElement.appendChild(imgElement)
+        divElement.appendChild(imgElement);
 
 
         const secondDiv = document.createElement('div');
-        divElement.appendChild(secondDiv)
+        divElement.appendChild(secondDiv);
 
 
         const firstPElement = document.createElement('p');
-        firstPElement.classList.add('caption')
+        firstPElement.classList.add('caption');
 
-        const d = new Date('Wednesday December 10 2019')
+        const d = new Date('Wednesday December 10 2019');
 
         firstPElement.textContent = `${d} by ${userName.value}`
         secondDiv.appendChild(firstPElement);
@@ -39,14 +39,14 @@ commentApp.formElement.addEventListener('submit', function (event) {
 
         const secondPElement = document.createElement('p');
         secondPElement.textContent = userComment.value;
-        secondDiv.appendChild(secondPElement)
+        secondDiv.appendChild(secondPElement);
 
 
         userName.value = '';
         userEmail.value = '';
         userComment.value = '';
     } else {
-        alert('Please fill out all fields')
+        alert('Please fill out all fields');
     }
 })
 
